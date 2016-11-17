894ebccdfb0f87eca4cd5b7f1e20637e16fd313a
billyluy <bluy0969@bths.edu>
2016-11-17 18:16:23 -0500

    Update BillyAi.java

--- CaveExplorer/src/core/BillyJiaMing/BillyAi.java
+++ CaveExplorer/src/core/BillyJiaMing/BillyAi.java
@@ -3,27 +3,27 @@ package core.BillyJiaMing;
 public class BillyAi {
 
 	public static String[][] placeShip() {
-		//String[][] map = EventBillyJiaMing.botMap;
+		//String[][] map = EventBillyJiaMing.botShip;
 		int rand1 = (int)Math.random()*10 +1;
 		int rand2 = (int)Math.random()*10 +1;
-		EventBillyJiaMing.botMap[rand1][rand2] = "X";
+		EventBillyJiaMing.botShip[rand1][rand2] = "X";
 		int randDir = (int)Math.random()*4 +1;
 		nextPos(randDir, rand1, rand2);
 			
-		return EventBillyJiaMing.botMap;
+		return EventBillyJiaMing.botShip;
 	}
 
 	private static void nextPos(int randDir, int x, int y) {
 		if(randDir == 1 && y-1>-1){
-			EventBillyJiaMing.botMap[x][y-1] = "X";
+			EventBillyJiaMing.botShip[x][y-1] = "X";
 		}else if(randDir ==2 && x-1>-1){
-			EventBillyJiaMing.botMap[x-1][y] = "X";
+			EventBillyJiaMing.botShip[x-1][y] = "X";
 		}
 		else if(randDir ==3 && y+1<11){
-			EventBillyJiaMing.botMap[x][y+1] = "X";
+			EventBillyJiaMing.botShip[x][y+1] = "X";
 		}
 		else if(randDir ==4 && x+1<11){
-			EventBillyJiaMing.botMap[x+1][y] = "X";
+			EventBillyJiaMing.botShip[x+1][y] = "X";
 		}
 	}
 
