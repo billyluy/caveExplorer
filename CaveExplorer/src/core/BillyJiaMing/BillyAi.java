05934fc9430c45fd3fd35dbfac87f03a7678757e
billyluy <bluy0969@bths.edu>
2016-11-17 12:34:38 -0500

    update

--- CaveExplorer/src/core/BillyJiaMing/BillyAi.java
+++ CaveExplorer/src/core/BillyJiaMing/BillyAi.java
@@ -3,8 +3,28 @@ package core.BillyJiaMing;
 public class BillyAi {
 
 	public static String[][] placeShip() {
-		// TODO NO PRINTING all AI
-		return null;
+		//String[][] map = EventBillyJiaMing.botMap;
+		int rand1 = (int)Math.random()*10 +1;
+		int rand2 = (int)Math.random()*10 +1;
+		EventBillyJiaMing.botMap[rand1][rand2] = "X";
+		int randDir = (int)Math.random()*4 +1;
+		nextPos(randDir, rand1, rand2);
+			
+		return EventBillyJiaMing.botMap;
+	}
+
+	private static void nextPos(int randDir, int x, int y) {
+		if(randDir == 1 && y-1>-1){
+			EventBillyJiaMing.botMap[x][y-1] = "X";
+		}else if(randDir ==2 && x-1>-1){
+			EventBillyJiaMing.botMap[x-1][y] = "X";
+		}
+		else if(randDir ==3 && y+1<11){
+			EventBillyJiaMing.botMap[x][y+1] = "X";
+		}
+		else if(randDir ==4 && x+1<11){
+			EventBillyJiaMing.botMap[x+1][y] = "X";
+		}
 	}
 
 	public static void updateUserHit() {
