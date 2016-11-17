cb03a189679e4032b96dca9cb346aef56dee867d
Student 6 <Student 6@10.8.33.164>
2016-11-17 12:26:56 -0500

    asdas

--- CaveExplorer/src/core/BillyJiaMing/JiaMingInput.java
+++ CaveExplorer/src/core/BillyJiaMing/JiaMingInput.java
@@ -1,10 +1,42 @@
 package core.BillyJiaMing;
 
+import core.CaveExplorer;
+
 public class JiaMingInput {
 
 	public static String[][] placeShip() {
-		// TODO print map user
-		return null;
+		boolean[] placedShip=new boolean[4];
+		String[] shipTypes={"Carrier(Length 5)","Battleship(Length 4)","Submarine(Length 3)","Destroyer(Length 2)"};
+		System.out.println("It's time to place your 4 ships.");
+		System.out.println("- - - Press Enter - - -");
+		CaveExplorer.in.nextLine();
+		while(stillShipLeft(placedShip)){
+			for(int i=0;i<4;i++){
+				if(placedShip[i]==false){
+					System.out.println("Enter "+i+" to place "+shipTypes[i]);
+				}
+			}
+			if(validInput(placedShip))
+			EventBillyJiaMing.printArr(EventBillyJiaMing.userMap);
+		}
+	}
+
+	private static boolean validInput(boolean[] placedShip) {
+		for(int i=0;i<placedShip.length;i++){
+			if(CaveExplorer.in.nextLine().indexOf(i)>-1 &&){
+				
+			}
+		}
+		return false;
+	}
+
+	private static boolean stillShipLeft(boolean[] placedShip) {
+		for(int i=0;i<placedShip.length;i++){
+			if(placedShip[i]==false){
+				return true;
+			}
+		}
+		return false;
 	}
 
 	public static void updateBotHit() {
