2dda408a4103b88b5b1b073ba39cf0d511759364
Jimmy <Jimmy@DESKTOP-95H47VE.fios-router.home>
2016-11-18 22:48:20 -0500

    xd

--- CaveExplorer/src/core/BillyJiaMing/JiaMingInput.java
+++ CaveExplorer/src/core/BillyJiaMing/JiaMingInput.java
@@ -3,46 +3,67 @@ package core.BillyJiaMing;
 import core.CaveExplorer;
 
 public class JiaMingInput {
-
+	private static int row; //y
+	private static int col; //x
+	private static String input;
+	
 	public static String[][] placeShip() {
-		boolean[] placedShip=new boolean[4];
+		int[] shipLength={5,4,3,2};
 		String[] shipTypes={"Carrier(Length 5)","Battleship(Length 4)","Submarine(Length 3)","Destroyer(Length 2)"};
-		System.out.println("It's time to place your 4 ships.");
-		System.out.println("- - - Press Enter - - -");
-		CaveExplorer.in.nextLine();
-		while(stillShipLeft(placedShip)){
-			for(int i=0;i<4;i++){
-				if(placedShip[i]==false){
-					System.out.println("Enter "+i+" to place "+shipTypes[i]);
-				}
+		CaveExplorer.print("It's time to place your 4 ships.");
+		CaveExplorer.print("- - - Press Enter - - -");
+		for(int i=0;i<shipTypes.length;i++){
+			CaveExplorer.print("Where do you want to place your "+shipTypes[i]+"? Pick a coord by typing \"(row,col)\"");
+			input=CaveExplorer.in.nextLine();
+			pickACoord();
+			pickDirectionToPlace();			
+		}
+		return null;
+		
+	}
+	
+	private static void pickDirectionToPlace() {
+		
+		
+	}
+
+	private static void pickACoord() {
+		boolean notValid=true;
+		while(notValid){
+			while(!validCoord()){
+				System.out.println("Enter a valid coord \"(row,col)\"");
+				input=CaveExplorer.in.nextLine();
 			}
-			String input = CaveExplorer.in.nextLine();
-			if(validInput(placedShip,input)){
-				EventBillyJiaMing.printArr(EventBillyJiaMing.userMap);
-				System.out.println("Where would you like to place the ship? Pick a coord(x,y)");
+			if(EventBillyJiaMing.userMap[row][col].equals(" ")){
+				notValid=false;
 			}
 		}
 	}
 
-	private static boolean validInput(boolean[] placedShip, String input) {
-		for(int i=0;i<placedShip.length;i++){
-			if(input.indexOf(i)>-1 && placedShip[i]==false){
-				return true
-			}
+	private static boolean validCoord() {
+		int commaIndex=input.indexOf(",");
+		String row=input.substring(commaIndex-1, commaIndex);
+		String col=input.substring(commaIndex+1, commaIndex+2);
+		if(isNumber(row) && isNumber(col)){
+			changeRowCol(col,row);
+			return true;
 		}
 		return false;
 	}
-
-	private static boolean stillShipLeft(boolean[] placedShip) {
-		for(int i=0;i<placedShip.length;i++){
-			if(placedShip[i]==false){
-				return true;
-			}
-		}
-		return false;
+	private static void changeRowCol(String sCol, String sRow) {
+		row=Integer.parseInt(sRow);
+		col=Integer.parseInt(sCol);
 	}
-
-	public static void updateBotHit() {
+	private static boolean isNumber(String str){
+		try{  
+			double d = Double.parseDouble(str);  
+		}  
+		catch(NumberFormatException err){ //if try statement returns this error, then return false 
+		    return false;  
+		}  
+		return true;  
+	}
+	private static void updateBotHit() {
 		// TODO this returns coords of place user picks
 		//CHEAT CODE
 		//EventBillyJiaMing.userHit[1][1];
