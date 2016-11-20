b3e7949782f6ee11a02e7a923107866a2c4bcd38
BillyLuy <bluy0969@bths.edu>
2016-11-20 16:03:01 -0500

    almost complete placeship

--- CaveExplorer/src/core/BillyJiaMing/BillyAi.java
+++ CaveExplorer/src/core/BillyJiaMing/BillyAi.java
@@ -7,43 +7,77 @@ public class BillyAi {
 	public static String[][] placeShip() {
 		//String[][] map = EventBillyJiaMing.botShip;
 		//USE 2 FOR LOOPS 1 FOR SHIP SIZE 1 FOR PLACE AMOUNT OF X
-		for(int i=5;i>4;i--){
-			int rand1 = (int)(Math.random()*10 +1);
-			int rand2 = (int)(Math.random()*10 +1);
-			while(EventBillyJiaMing.botShip[rand1][rand2].equals("1")){
-				CaveExplorer.print("why u run");
-				rand1 = (int)(Math.random()*10 +1);
-				rand2 = (int)(Math.random()*10 +1);
-			}
-			EventBillyJiaMing.botShip[rand1][rand2] = "1";
+		for(int i=2;i<6;i++){
+			int rand1 = (int)(Math.random()*10);
+			int rand2 = (int)(Math.random()*10);
 			int randDir = (int)(Math.random()*4 +1);
+			//validPos(i,rand1,rand2,randDir)
+			while(EventBillyJiaMing.botShip[rand1][rand2].equals("X")){
+				rand1 = (int)(Math.random()*10);
+				rand2 = (int)(Math.random()*10);
+			}
+			System.out.print(randDir+" ");
+			CaveExplorer.print(rand2+" "+rand1);
+			EventBillyJiaMing.botShip[rand1][rand2] = "X";
 			nextPos(randDir, rand1, rand2, i);
 		}
-		
-			
 		return EventBillyJiaMing.botShip;
+	}
+
+
+	private static boolean validPos(int size, int rand1, int rand2,int dir) {
+		for(int i =0;i<size;i++){
+			if(dir ==1 &&EventBillyJiaMing.botShip[rand1-i][rand2].equals("X")){
+				return true;
+			}else if(dir ==2 &&EventBillyJiaMing.botShip[rand1][rand2+i].equals("X")){
+				return true;
+			}else if(dir ==3 &&EventBillyJiaMing.botShip[rand1+i][rand2].equals("X")){
+				return true;
+			}else if(dir ==4 &&EventBillyJiaMing.botShip[rand1][rand2-i].equals("X")){
+				return true;
+			}
+		}
+		return false;
 	}
 
 
 	private static void nextPos(int randDir, int currX, int currY, int count) {
 		//&& !EventBillyJiaMing.botShip[x][y--].equals("X")
+		int dirPick = randDir;
 		int x = currX;
 		int y = currY;
-		if(randDir == 1 || randDir == 3){
-			if(currY-count>-1){
-				y-= count;
-			}else{
-				y+= count;
+		for(int i = count-1; i>0;i--){
+			if(dirPick ==1){
+				if(x-1>-1){
+					x-= 1;
+				}else{
+					x+=(x+currX)+1;
+					dirPick = 3;
+				}
+			}else if(dirPick == 3){ 
+				if(x+1<10){
+					x+= 1;
+				}else{
+					x-=(x-currX)+1; 
+					dirPick = 1;
+				}
+			}else if(dirPick == 2){
+				if(y+1<10){
+					y+= 1;
+				}else{
+					y-=(y-currY)+1; 
+					dirPick = 4;
+				}
+			}else if(dirPick == 4){ 
+				if(y-1>-1){
+					y-= 1;
+				}else{
+					y+=(y+currY)+1;
+					dirPick = 2;
+				}
 			}
+			EventBillyJiaMing.botShip[x][y] = "X";
 		}
-		if(randDir == 2 || randDir == 4){
-			if(currX-count>-1){
-				x-= count;
-			}else{
-				x+= count;
-			}
-		}
-		EventBillyJiaMing.botShip[x][y] = "2";
 	}
 	public static void updateUserHit() {
 		// TODO Auto-generated method stub
