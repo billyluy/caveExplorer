56ab0609951988b00cfccdc942f621ed502915bb
billyluy <bluy0969@bths.edu>
2016-11-22 12:39:12 -0500

    Update GameStartEvent.java

--- CaveExplorer/src/core/GameStartEvent.java
+++ CaveExplorer/src/core/GameStartEvent.java
@@ -3,10 +3,8 @@ package core;
 
 public class GameStartEvent implements Event {
 	
-	public static final String[] SEQ_1 = {"A piky-haired guy with orange pants files up to you.",
-			"Have you Arrived to save us from the treacherous Buu?","We need your help."};
-	public static final String[] SEQ_2 = {"Great, I will be busy meditating on my super yellow version while you figure out where Buu is",
-			"KAAAAA, MEEEEEE, HAAAAA, MEEEE HAAAAAAAAAAAAAAAAAAAAAA","By the way, here is a map"};
+	public static final String[] SEQ_1 = {"While in the cave", "You have to beat the three puzzles to get rid of the ghost",
+			"Here is a map to guide you out","You will bot be able to exit unless you have beat all the puzzles!"};
 
 	public GameStartEvent() {
 		
@@ -14,12 +12,6 @@ public class GameStartEvent implements Event {
 
 	public void play() {
 		readSequence(SEQ_1);
-		CaveExplorer.print("Will you help us?");
-		while(CaveExplorer.in.nextLine().indexOf("yes")<0){
-			CaveExplorer.print("Please say yes. I beg you!");
-		}
-		readSequence(SEQ_2);
-		//gives the user the map
 		CaveExplorer.inventory.setHasMap(true);
 	}
 
